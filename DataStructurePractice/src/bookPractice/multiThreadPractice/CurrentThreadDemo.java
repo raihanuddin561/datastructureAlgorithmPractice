b7c7f37d4cb1ebe0b7916e7b4b3564a4f61b6a85
raihanuddin561 <raihanuddin561@gmail.com>
2022-03-15 15:51:53 +0600

    interface practice

--- DataStructurePractice/src/bookPractice/multiThreadPractice/CurrentThreadDemo.java
+++ DataStructurePractice/src/bookPractice/multiThreadPractice/CurrentThreadDemo.java
@@ -1,5 +1,18 @@
 package bookPractice.multiThreadPractice;
 
 public class CurrentThreadDemo {
-
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+        System.out.println("Current Thread: "+t);
+        t.setName("Main Thread");
+        System.out.println("after name changed: "+t);
+        try {
+            for(int n=5;n>0;n--){
+                System.out.println(n);
+                Thread.sleep(1000);
+            }
+        }catch (InterruptedException e){
+            System.out.println("main thread");
+        }
+    }
 }
